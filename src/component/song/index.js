7e08bde8bb4c9d129f776da3b88b3bd43d5b5f2c
mediallisar <medialisar99@gmail.com>
2022-03-25 11:25:14 +0700

    Tugas 02 Module 02

---
+++ src/component/song/index.js
@@ -0,0 +1,20 @@
+const SongComponent = ({ images, title, artist}) => {
+    return (
+        <div class="list">
+            <div class="list-img">
+                <img
+                id="song-image"
+                src={ images }
+                alt="Image"
+                />
+            </div>
+            <div class="list-detail">
+                <p class="song-title">{ title }</p>
+                <p class="song-artist">{ artist }</p>
+                <input type="button" value="Select" class="custom-btn btn-2" />
+            </div>
+        </div>
+    );
+};
+
+export default SongComponent;
